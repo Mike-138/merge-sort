20802678bcd63a81997195de08d92f8d65e1ca3c
Michael <mikemcd.bbs@gmail.com>
2023-11-10 16:35:47 -0500

    Run prettier on files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
-  extends: [
-    'semistandard',
-    'prettier'
-  ]
+  extends: ["semistandard", "prettier"],
 };
--- merge_sort.js
+++ merge_sort.js
@@ -1,37 +1,37 @@
 const mergeSort = (array) => {
-    const merge = (leftArray, rightArray) => {
-        const mergedArray = [];
-        let leftPointer = 0;
-        let rightPointer = 0;
-        while (leftPointer < leftArray.length || rightPointer < rightArray.length) {
-            if (!(leftPointer < leftArray.length)) {
-                mergedArray.push(rightArray[rightPointer]);
-                rightPointer += 1;
-                continue;
-            } else if ((!(rightPointer < rightArray.length))) {
-                mergedArray.push(leftArray[leftPointer]);
-                leftPointer += 1;
-                continue;
-            } else if (leftArray[leftPointer] < rightArray[rightPointer]) {
-                mergedArray.push(leftArray[leftPointer]);
-                leftPointer += 1;
-            } else {
-                mergedArray.push(rightArray[rightPointer]);
-                rightPointer += 1;
-            }
-        }
-        return mergedArray;
+  const merge = (leftArray, rightArray) => {
+    const mergedArray = [];
+    let leftPointer = 0;
+    let rightPointer = 0;
+    while (leftPointer < leftArray.length || rightPointer < rightArray.length) {
+      if (!(leftPointer < leftArray.length)) {
+        mergedArray.push(rightArray[rightPointer]);
+        rightPointer += 1;
+        continue;
+      } else if (!(rightPointer < rightArray.length)) {
+        mergedArray.push(leftArray[leftPointer]);
+        leftPointer += 1;
+        continue;
+      } else if (leftArray[leftPointer] < rightArray[rightPointer]) {
+        mergedArray.push(leftArray[leftPointer]);
+        leftPointer += 1;
+      } else {
+        mergedArray.push(rightArray[rightPointer]);
+        rightPointer += 1;
+      }
     }
+    return mergedArray;
+  };
 
-    if (array.length < 2) {
-        return array;
-    }
-    const mid = Math.floor(array.length / 2);
-    const left = mergeSort(array.slice(0, mid));
-    const right = mergeSort(array.slice(mid))
-    return merge(left, right);
-}
+  if (array.length < 2) {
+    return array;
+  }
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+  return merge(left, right);
+};
 
 module.exports = {
-    mergeSort
-}
+  mergeSort,
+};
